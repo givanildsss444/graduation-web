9f80bb4b4d41755980108edf6992af4ca7057be5
givanildsss444 <cicerofarias1390gmil@gmail.com>
2025-05-20 14:16:57 -0300

    #8.1.5
    👍👍👍👍👍

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { IoArrowDownCircleOutline } from "react-icons/io5";
 
 import './App.css'
 
-export default function Home() {
+export default function App() {
 
   return(
 
--- src/components/barNavegation.jsx
+++ src/components/barNavegation.jsx
@@ -1,16 +1,13 @@
 import './styles/barNavegation.css'
 
-import { Link } from 'react-router-dom'
-
 export default function NavegationBar() {
 
     return(
 
         <div className="bar-navegationFull">
 
-            <Link to='https://formaturaifal2026.netlify.app'>
-                <a href="#date">Início</a>
-            </Link>
+          
+            <a href="#date">Início</a>
             <a href="#class">Turma</a>
             <a href="#thanks">Agradecimentos</a>
             <a href="#qr-code">Doe</a>
